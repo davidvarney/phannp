afc5279dc4cfed835bcd9c3a73bdf3b0fc9e0f8f
David Varney <davidvarney@gmail.com>
2025-10-08 23:06:08 -0400

    :rotating_light: CodeSniffer
    On branch refactor-resources
     Changes to be committed:
    	modified:   src/Resources/Account.php

--- src/Resources/Account.php
+++ src/Resources/Account.php
@@ -6,13 +6,13 @@ class Account extends Resource
 {
     /**
      * Get details about the authenticated user
-     * 
+     *
      * This is not a part of the Stannp Account API documentation.
      * This is found under the Authentication section of the API docs.
      * The docs for this can be found at the below link
      * @link https://www.stannp.com/us/direct-mail-api/guide#authentication
-     * 
-     * 
+     *
+     *
      * @return array
      * @throws \Phannp\Exceptions\ApiException
      * @throws \GuzzleHttp\Exception\GuzzleException
