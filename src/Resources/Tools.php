a463216444fd3a2bb7f36de703aabe7113963049
David Varney <davidvarney@gmail.com>
2025-10-10 00:20:23 -0400

    :recycle: Refactors the Tools resource
    On branch refactor-resources
     Changes to be committed:
    	modified:   src/Resources/Tools.php
    	modified:   tests/Resources/ToolsTest.php

--- src/Resources/Tools.php
+++ src/Resources/Tools.php
@@ -5,20 +5,60 @@ namespace Phannp\Resources;
 class Tools extends Resource
 {
     /**
-     * @todo These are NOT correct endpoints - need to confirm with Stannp
+     * Generate a QR code image URL with specified data and size.
+     *
+     * @link https://www.stannp.com/us/direct-mail-api/tools#qrcode
+     *
+     * @param string $data The data to encode in the QR code
+     * @param int $size The size of the QR code image (default: 200)
+     * @return array
+     * @throws \Phannp\Exceptions\ApiException on HTTP or API errors
      */
-    public function getCountries(): array
+    public function qrcodeCreate(string $data, int $size = 200): array
     {
-        return $this->client->get('countries/list');
+        return $this->client->get('qrcode/create', [
+            'data' => $data,
+            'size' => $size,
+        ]);
     }
 
-    public function getRegions(string $countryCode): array
+    /**
+     * Merge multiple PDF files into a single PDF.
+     *
+     * @link https://www.stannp.com/us/direct-mail-api/tools#merge-pdf
+     *
+     * @param array $files An array of file paths or URLs to merge
+     * @return array
+     * @throws \Phannp\Exceptions\ApiException on HTTP or API errors
+     */
+    public function pdfMerge(array $files): array
     {
-        return $this->client->get('regions/list', ['country' => $countryCode]);
+        // Client-side validation: require a non-empty array of strings
+        if (empty($files)) {
+            throw new \InvalidArgumentException('files must be a non-empty array of file paths or URLs');
+        }
+
+        foreach ($files as $f) {
+            if (!is_string($f) || trim($f) === '') {
+                throw new \InvalidArgumentException('each file must be a non-empty string');
+            }
+        }
+
+        return $this->client->get('pdf/merge', [
+            'files' => $files,
+        ]);
     }
 
-    public function getPricing(): array
+    /**
+     * Retrieve a list of available templates.
+     *
+     * @link https://www.stannp.com/us/direct-mail-api/tools#templates
+     *
+     * @return array
+     * @throws \Phannp\Exceptions\ApiException on HTTP or API errors
+     */
+    public function getTemplates(): array
     {
-        return $this->client->get('pricing');
+        return $this->client->get('templates/list');
     }
 }
