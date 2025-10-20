ad8e3e585fbf9fe6f2eb0212a690fc77c7aeb67a
David Varney <davidvarney@gmail.com>
2025-10-20 16:05:05 -0400

    :bug: Updates the Postcard::create() method
    Realized the create method only accepted a size param which is just wrong. Updated the create method to now accept a generic array and we make sure that we throw when the size param isn;t quite correct.
    
    On branch fix-postcard-create-method
     Changes to be committed:
    	modified:   README.md
    	modified:   src/Resources/Postcards.php
    	modified:   tests/Resources/PostcardsTest.php

--- src/Resources/Postcards.php
+++ src/Resources/Postcards.php
@@ -9,16 +9,22 @@ class Postcards extends Resource
      *
      * @link https://www.stannp.com/us/direct-mail-api/postcards#createPostcard
      *
-     * @param string $size The size of the postcard, e.g., '4x6', '6x9', '6x11'
+     * @param array $data An array of options. Options must include 'size' => '4x6'|'6x9'|'6x11', etc.
+     *
+     * Changelog:
+     * - 2025-10-20: `create()` now requires an associative array and must include a 'size' key
+     *               (e.g. `['size' => '4x6']`). Previous behavior accepting a bare size string
+     *               was removed to enforce explicit parameter passing.
      * @todo I know there are more parameters here, but the docs are not clear.
      *       I'm thinking that their API docs/site are under construction at the
      *       moment or there's not much care about it.
      */
-    public function create(string $size): array
+    public function create(array $data): array
     {
-        $data = [
-            'size' => $size,
-        ];
+        // Require a size to be present.
+        if (!isset($data['size']) || empty($data['size'])) {
+            throw new \InvalidArgumentException('size must be a valid postcard size, EX: "4x6", "6x9", "6x11"');
+        }
 
         return $this->client->post('postcards/create', $data);
     }
